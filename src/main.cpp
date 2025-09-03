c474bc5c766f94a7ee8573c5402d96f4d9016a81
marcel <marcel@machine>
2025-09-03 02:11:03 +0200

    rmeoved pattern matching lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,31 @@
+// declarations
 #include <cstdio>
-#include <iostream>
 #include <list>
 #include <matchit.h>
-#include <type_traits>
+#include <print>
 #include <variant>
 
+// imports
 using matchit::_;
 using matchit::as;
 using matchit::match;
 using matchit::pattern;
 
 enum class KeywordKind {
+  And,
+  Or,
+  Not,
+
+  Equals,
+  NotEquals,
+
+  Greater,
+  Smaller,
+
+  If,
+  Colon,
+
+  Assignment,
 
 };
 
@@ -19,9 +34,20 @@ using Token = std::variant<double, int, KeywordKind>;
 auto main(const int argc, const char *const *const argv) noexcept -> int {
   std::list<Token> tokens;
 
+  const auto f = [&]<typename T>() {};
+
   for (const auto &token : tokens) {
-    match(token)(
-        pattern | as<KeywordKind>(_) = [&]() -> void { printf("test"); },
-        pattern | as<double>(_) = [&]() -> void {  });
+    std::visit(
+        [&]<typename T>(T &&value) {
+          if constexpr (std::is_same_v<T, int>) {
+            std::println("int: {}", value);
+          }
+          if constexpr (std::is_same_v<T, double>) {
+            std::println("double: {}", value);
+          }
+          if constexpr (std::is_same_v<T, KeywordKind>) {
+          }
+        },
+        token);
   }
 }
